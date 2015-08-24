c0bdb630369f5a9f8620353133236c4172409a9e
Nick Smith <nick.smith@cern.ch>
2015-08-24 13:13:29 +0200

    Put in GHM CMSlumi for now

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
 import ROOT
+import os
+
+# Package macro path
+packagePath = os.path.dirname(os.path.realpath(__file__))
+ROOT.gROOT.SetMacroPath(ROOT.gROOT.GetMacroPath() + os.path.join(packagePath, 'macro') + ':')
 
 from CMSlumi import CMSlumi
+ROOT.CMSlumi = CMSlumi
+ROOT.gROOT.ProcessLineSync(".L CMS_lumi.C")
---
+++ macro/CMS_lumi.C
@@ -0,0 +1,13 @@
+#include "TPython.h"
+#include "TPad.h"
+#include "TString.h"
+
+int CMS_lumi(TPad* pad, int iPeriod, int iPosX) {
+  // TODO
+  // TString statement = "dotroot.CMSPlotDecorations.CMSlumi(%s, %d, %d)";
+  // statement.sprintf(...);
+  // int returnValue = TPython::Eval(statement);
+  // return returnValue;
+  return -1;
+}
+
